1d745b69dfafb0cbe60df3b1d94ddf5052789c35
EarlOld <dimonsuper010@gmail.com>
2017-02-09 14:31:02 +0200

    1

--- lab-3.1/main.cpp
+++ lab-3.1/main.cpp
@@ -1,83 +1,94 @@
-#include <iostream> 
-#include <cmath> 
+#include <iostream>
 
 using namespace std;
-
-void print(int **Arr, int m, int n)
-{
-	cout << endl << "Elements array: ";
-	for(int i = 0; i < n; i++)
+void Input(int **A, int n, int m){
+	
+	int i,j;
+	
+	for(i=0;i<n;i++)
+	for(j=0;j<m;j++)
 	{
-		cout << endl;
-		for(int j = 0; j < m; j++)
-		cout <<  Arr[i][j] << "\t";
+		 cout << "Vvedite a[" << i << "][" << j << "]: " ; 
+		cin >>A[i][j]; 
 	}
 	
+	
 }
 
-void input(int **Arr, int m, int n)
-{
-	cout << "Input element array:";
-	for(int i = 0; i < n; i++)
-		for(int j = 0; j < m; j++)
-		cin >> Arr[i][j];
+void Output(int **A, int n,int m){
 	
-}
-void temp(int **Arr, int i, int m)
-{
-	int temp; 
-	for(int j = 0; j < m; j++)
-	{
-		temp = Arr[i +1][j];
-		Arr[i + 1][j] = Arr[i][j];
-		Arr[i][j] = temp;
+	int i,j;
 	
+	for(i=0;i<n;i++){
+	printf("\n");	
+	for(j=0;j<m;j++)
+	printf("\t%5d",A[i][j]);
 	}
 }
-void sort(int **Arr, int m, int n)
-{
-	int *Arr1 = new int[n];
-		for(int i = 0; i < n; i++)
-			for(int j = 0; j < m; j++)
-				if(Arr[i][j] % 2 != 0 && Arr[i][j] < 0)
-					Arr1[i] += Arr[i][j];
 
-		for(int c = 0; c < n; c++)
-			for(int i = 0; i < n; i++)
-				for(int j = 0; j < m; j++)
-					if(Arr1[i] > Arr1[i + 1])
-						temp(Arr, i, m);
+int Sum(int **A, int n, int j){
+	
+	int i,S=0;
+	
+	for(i=0;i<n;i++)
+	{
+		if(A[i][j]<0 && A[i][j]%2!=0)
+		{
+			S+=abs(A[i][j]);
+		}
+	}
+	
+	return S;
+}
 
-					
+void Sort(int **A, int n, int m){
+	
+	int i,l,j,S1,S2,a;
+	
+	for(l=0;l<n;l++)
+	for(j=1;j<m;j++)
+	{
+		S1=Sum(A,n,j);
+		S2=Sum(A,n,j-1);
 		
-
+		for(i=0;i<n;i++)
+		if(S1<S2)
+		{
+			a=A[i][j];
+			A[i][j]=A[i][j-1];
+			A[i][j-1]=a;
+		}
+	}
+	
+	
 }
-int main ()
-{
-	system("color f0");
 
-	int n = 0, m = 0;
-	cout << "Input count array[n][m]:";
+int main() {
+	
+	system("color f0");
+	int **A,i,j,n,m;
+	
+	cout << "Input size matrix A[n][m]:" << endl;
 	cin >> n >> m;
 	
-	int **Arr = new int* [n];
-		for(int i = 0; i < n; i++)
-			Arr[i] = new  int[m];
-
+	A=(int**)malloc(sizeof(int)*n);
+	for(i=0; i<n; i++)
+	A[i]=(int*)malloc(sizeof(int)*m);
+	
+	Input(A,n,m);
+	cout << endl << "Matrix:" << endl;
+	Output(A,n,m);
 	
 
-
+	cout << endl << "Result:" << endl;
+	Sort(A,n,m);
+	Output(A,n,m);
 	
+	for(i=0; i<n; i++)
+	free(A[i]);
+	free(A);
 
-	input(Arr, m, n);
-	print(Arr, m, n);
-	sort(Arr, m, n);
-	print(Arr, m, n);
-
-	for(int i = 0; i < n; i++)
-		delete Arr[i];
-	delete Arr;
-
+	cout << endl;
 	system("pause");
 	return 0;
 }
