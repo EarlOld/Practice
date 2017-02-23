9670474fbb3d1e8849f8d602366b4d4215af50c0
EarlOld <dimonsuper010@gmail.com>
2017-02-23 22:15:57 +0200

    Лабораторна 9. Стек - шаблон

---
+++ Week_three_lab_9/main.cpp
@@ -0,0 +1,14 @@
+
+
+
+
+#include "stack.h"
+
+int main()
+{
+	system("color f0");
+	Stack<char> St;
+	St.SwichMenu();
+	system("pause");
+	return 0;
+}
---
+++ Week_three_lab_9/stack.h
@@ -0,0 +1,166 @@
+#include <string>
+#include <iostream>
+using namespace std;
+template<class T>
+class Stack
+{
+public:
+	Stack() :tail(0), head(0)
+	{
+	}
+
+	~Stack()
+	{
+		while (head)
+		{
+			tail = head->next;
+			delete head;
+			head = tail;
+		}
+	}
+
+	void push(T val)
+	{
+		Node* Temp;
+		Temp = new Node;
+		Temp->elem = val;
+		if (tail == 0)
+		{
+			tail = Temp;
+		}
+		else
+		{
+			Temp->next = tail;
+			tail = Temp;
+		}
+		size++;
+	}
+
+	T top()
+	{
+		if (tail == 0)
+		{
+			throw string("Stack is empty!");
+		}
+		return tail->elem;
+	}
+
+	void pop()
+	{
+		while (tail != 0)
+		{
+			Node* delptr = tail;
+			tail = tail->next;
+			delete delptr;
+		}
+		
+		throw string("Stack is empty!");
+	}
+
+	void print()
+	{
+		if (tail == 0)
+		{
+			throw string("Stack is empty!");
+		}
+		for (Node* ptr = tail; ptr != 0; ptr = ptr->next)
+		{
+			std::cout << ptr->elem << ' ';
+		}
+		std::cout << '\n';
+	}
+	void getIndex(int q)
+	{
+		if (q > size)
+		{
+			cout << endl << "Error!" << endl;
+		}
+		if (tail == 0)
+		{
+			throw string("Stack is empty!");
+		}
+		int flag = 0;
+		for (Node* ptr = tail; ptr != 0; ptr = ptr->next)
+		{
+			flag++;
+			if(flag == q) cout << "Elem index " << q << " : " << ptr->elem << endl;
+		}
+		
+	}
+	void SwichMenu();
+	inline void OutputMenu()
+	{
+		cout << "\t Select an action" << endl
+			<< "1 - Clean the stack;" << endl
+			<< "2 - Adding an element to the end of the stack;" << endl
+			<< "4 - Getting a pointer to the i-th element of the stack;" << endl
+			<< "5 - Get stack item ;" << endl
+			<< "6 - Print the list;" << endl
+			<< "0 - Exit." << endl;
+	}
+
+private:
+	struct Node
+	{
+		Node() :elem(0), next(0)
+		{
+		}
+		Node* next;
+		T elem;
+	};
+	Node* head;
+	Node* tail;
+	int size;
+};
+template <typename T>
+void Stack<T>::SwichMenu()
+{
+	int action;
+	while (true)
+	{
+		OutputMenu();
+		cout << endl << "Select an action: ";
+		cin >> action;
+		switch (action)
+		{
+			;
+			
+		case 1:
+			pop();
+			break;
+		case 2:
+			T a;
+			cout << "INtput element: ";
+			cin >> a;
+			push(a);
+			break;
+		case 4:
+			cout << "Enter number element: ";
+			int elem;
+			cin >> elem;
+			if (elem <= 0)
+			{
+				cerr << "\t Incorrect input" << endl;
+				break;
+			}
+			getIndex(elem);
+			break;
+		case 5:
+			cout << "Element: " << top() << endl;
+			
+			break;
+		case 6:
+		{
+			print();
+			break;
+		}
+		
+		case 0: return;
+		default:
+			cerr << "\t Incorrect input" << endl;
+			break;
+		}
+		system("pause");
+		system("cls");
+	}
+}
